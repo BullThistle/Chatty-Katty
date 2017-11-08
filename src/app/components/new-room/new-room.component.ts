c590bfa1eec44af50cf20b85c2dc8c4a669441e0
Kelsey Langlois <kels.langlois@gmail.com>
2017-11-08 15:35:24 -0800

    Update new room to save creator name and id.

--- src/app/components/new-room/new-room.component.ts
+++ src/app/components/new-room/new-room.component.ts
@@ -4,6 +4,7 @@ import { User } from '../models/user.model';
 import { ChatMessage } from '../models/chat-message.model';
 import { ChatRoom } from '../models/chat-room.model';
 import { RoomService } from '../../room.service';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-new-room',
@@ -14,16 +15,26 @@ import { RoomService } from '../../room.service';
 
 export class NewRoomComponent implements OnInit {
   roomName: string = '';
+  userName: string;
+  userID: string;
 
-  constructor(private roomService: RoomService) { }
+  constructor(private roomService: RoomService, public authService: AuthService) {
+    this.authService.user.subscribe(user => {
+      if (user != null) {
+        this.userName = user.displayName;
+        this.userID = user.uid;
+      }
+    });
+  }
 
   ngOnInit() {
   }
 
   addRoom(value: any) {
-    var newRoom = new ChatRoom(value.roomName, "New Room Created", Date.now());
+    var newRoom = new ChatRoom(value.roomName, `${this.userName} created new room "${value.roomName}"`, Date.now());
+    var newRoomMessage = new ChatMessage(newRoom.lastMessage, newRoom.timestamp, this.userName, this.userID)
     this.roomName = '';
-    this.roomService.addRoom(newRoom);
+    this.roomService.addRoom(newRoom, newRoomMessage);
   }
 
 }
