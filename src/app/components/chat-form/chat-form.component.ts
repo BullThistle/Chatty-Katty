e56a8b717210156486a0363564794b3e5b44fade
Guest User <Guest@epicodus-39.local>
2017-11-08 15:11:30 -0800

    Add username to new messages.

--- src/app/components/chat-form/chat-form.component.ts
+++ src/app/components/chat-form/chat-form.component.ts
@@ -2,23 +2,32 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 import { ChatMessage } from '../models/chat-message.model';
 import { User } from '../models/user.model';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-chat-form',
   templateUrl: './chat-form.component.html',
-  styleUrls: ['./chat-form.component.css']
+  styleUrls: ['./chat-form.component.css'],
 })
 export class ChatFormComponent implements OnInit {
+  user;
+  userName: string;
   messageText: string = '';
   @Output() newMessage = new EventEmitter();
 
-  constructor() { }
+  constructor(public authService: AuthService) {
+    this.authService.user.subscribe(user => {
+      if (user != null) {
+        this.userName = user.displayName;
+      }
+    });
+  }
 
   ngOnInit() {
   }
 
   sendMessage(value: any) {
-    var message = new ChatMessage(value.messageText, Date.now(), "anonymous");
+    var message = new ChatMessage(value.messageText, Date.now(), this.userName);
     this.messageText = '';
     this.newMessage.emit(message);
   }
